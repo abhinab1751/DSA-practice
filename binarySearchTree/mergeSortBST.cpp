91a2f27055eedc8fbd14975542f6f9dffb70f964
abhinab <abhinab1751@gmail.com>
2026-02-22 12:52:58 +0530

    mergefull

--- binarySearchTree/mergeSortBST.cpp
+++ binarySearchTree/mergeSortBST.cpp
@@ -39,6 +39,69 @@ Node* buildBST(vector<int> arr){
     return root;
 }
 
+void inorder(Node* root, vector<int> &arr){
+    if(root == NULL){
+        return;
+    }
+    inorder(root -> left , arr);
+    arr.push_back(root -> data);
+    inorder(root -> right , arr);
+}
+
+Node* buildBSTFromSorted(vector<int> arr, int st , int end){
+    if(st > end){
+        return NULL;
+    }
+
+    int mid = st + (end-st)/2;
+    Node* root = new Node(arr[mid]);
+
+    root -> left = buildBSTFromSorted(arr, st , mid-1);
+    root -> right = buildBSTFromSorted(arr, mid+1, end);
+
+    return root;
+}
+
 Node* merge2BST(Node* root1, Node* root2){
-    
+    vector<int> arr1 , arr2;
+    inorder(root1, arr1);
+    inorder(root2, arr2);
+
+    vector<int >temp;
+
+    int i= 0, j=0;
+
+    while(i < arr1.size() && j < arr2.size()){
+        if(arr1[i] < arr2[j]){
+            temp.push_back(arr1[i++]);
+        } else {
+            temp.push_back(arr2[j++]);
+        }
+    }
+    while(i < arr1.size()){
+        temp.push_back(arr1[i++]);
+    }
+
+    while(j < arr2.size()){
+        temp.push_back(arr2[j++]);
+    }
+
+    return buildBSTFromSorted(temp, 0 , temp.size() - 1);
+}
+
+int main(){
+    vector<int> arr1 = {8,2,1,10};
+    vector<int> arr2 = {5,3,0};
+
+    Node* root1 = buildBST(arr1);
+    Node* root2 = buildBST(arr2);
+
+    Node* root = merge2BST(root1, root2);
+    vector<int> seq;
+    inorder(root, seq);
+    for(int v : seq){
+        cout << v << " ";
+    }
+    cout << endl;
+    return 0;
 }
