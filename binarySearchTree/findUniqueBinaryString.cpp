d5469921b6d01eb2b9e160f1557335b7173bb18e
abhinab <abhinab1751@gmail.com>
2026-03-11 23:56:00 +0530

    bitwiseComplement

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -59,3 +59,21 @@ public:
         return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
+
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+
+        int mask = 0;
+        int temp = n;
+
+        while(temp > 0){
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+
+        return mask ^ n;
+    }
+};
