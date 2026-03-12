64e9c0bf4527af14d75b36018430cb772b1ae18e
abhinab <abhinab1751@gmail.com>
2026-03-12 22:54:59 +0530

    maxStability

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -63,7 +63,7 @@ public:
 
 class Solution {
 public:
-    int bitwiseComplement(int n) {
+    int git (int n) {
         if(n == 0) return 1;
 
         int mask = 0;
@@ -77,3 +77,101 @@ public:
         return mask ^ n;
     }
 };
+
+
+class DSU {
+public:
+    vector<int> parent, rank;
+    
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+    
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);
+    }
+    
+    bool unite(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b) return false;
+        
+        if(rank[a]<rank[b]) swap(a,b);
+        parent[b]=a;
+        if(rank[a]==rank[b]) rank[a]++;
+        
+        return true;
+    }
+};
+
+class Solution {
+public:
+    
+    bool check(int n, vector<vector<int>>& edges, int k, long long x){
+        DSU dsu(n);
+        int upgrades=0;
+        int used=0;
+        
+        vector<vector<int>> opt;
+        
+        for(auto &e:edges){
+            int u=e[0],v=e[1],s=e[2],must=e[3];
+            
+            if(must){
+                if(s<x) return false;
+                if(dsu.unite(u,v)) used++;
+                else return false;
+            }else{
+                opt.push_back(e);
+            }
+        }
+        
+        vector<vector<int>> upg;
+        
+        for(auto &e:opt){
+            int u=e[0],v=e[1],s=e[2];
+            
+            if(s>=x){
+                if(dsu.unite(u,v)) used++;
+            }
+            else if(2LL*s>=x){
+                upg.push_back(e);
+            }
+        }
+        
+        for(auto &e:upg){
+            if(upgrades==k) break;
+            
+            int u=e[0],v=e[1];
+            if(dsu.unite(u,v)){
+                upgrades++;
+                used++;
+            }
+        }
+        
+        return used==n-1;
+    }
+    
+    
+    int maxStability(int n, vector<vector<int>>& edges, int k) {
+        
+        long long l=0,r=2e5,ans=-1;
+        
+        while(l<=r){
+            long long mid=(l+r)/2;
+            
+            if(check(n,edges,k,mid)){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
