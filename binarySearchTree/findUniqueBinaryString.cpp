ca58a8c0116d58d381cfd72f3a4498b01c7cacfa
abhinab <abhinab1751@gmail.com>
2026-03-13 23:22:22 +0530

    minNumberOfSeconds

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -175,3 +175,36 @@ public:
     }
 };
 
+
+
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long left = 1, right = 1e16, ans = right;
+
+        auto can = [&](long long t){
+            long long total = 0;
+
+            for(long long w : workerTimes){
+                long long x = (-1 + sqrt(1 + 8.0 * t / w)) / 2;
+                total += x;
+                if(total >= mountainHeight) return true;
+            }
+
+            return total >= mountainHeight;
+        };
+
+        while(left <= right){
+            long long mid = left + (right - left) / 2;
+
+            if(can(mid)){
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
