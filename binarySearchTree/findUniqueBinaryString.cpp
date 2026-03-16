d06007c369011d0fc0379c6b3ea9053a68f00b25
abhinab <abhinab1751@gmail.com>
2026-03-16 23:52:08 +0530

    getBiggestThree

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -233,3 +233,47 @@ public:
         return res[k-1];
     }
 };
+
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        set<int> sums;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                sums.insert(grid[i][j]); 
+
+                for(int k=1;;k++){
+                    if(i-k<0 || i+k>=m || j-k<0 || j+k>=n) break;
+
+                    int sum = 0;
+
+                    int x = i-k, y = j;
+
+                    
+                    for(int t=0;t<k;t++) sum += grid[x+t][y+t];
+
+
+                    for(int t=0;t<k;t++) sum += grid[i+t][j+k-t];
+
+                   
+                    for(int t=0;t<k;t++) sum += grid[i+k-t][j-t];
+
+                    
+                    for(int t=0;t<k;t++) sum += grid[i-t][j-k+t];
+
+                    sums.insert(sum);
+                }
+            }
+        }
+
+        vector<int> ans;
+        for(auto it = sums.rbegin(); it!=sums.rend() && ans.size()<3; it++)
+            ans.push_back(*it);
+
+        return ans;
+    }
+};
