4d3a9c67d8c1f5b603990d1fe43e697403163cc8
abhinab <abhinab1751@gmail.com>
2026-03-25 23:10:57 +0530

    detectCycledirected

---
+++ graph/rottingOrange.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int> *l;
+
+    public:
+    Graph(int V){
+        this->V = V;
+        l = new list<int> [V];
+    }
+    void addEdge(int u , int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void printAdjList() {
+        for(int i = 0;i<V;i++){
+            cout<< i << " : ";
+            for(int neigh : l[i]) {
+                cout << neigh << " ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
