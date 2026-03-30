960d3c83afb967e24c60dcb715c1087006e75722
abhinab <abhinab1751@gmail.com>
2026-03-30 17:39:16 +0530

    pattern7

---
+++ patterns/pattern7.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print1(){
+    for(int i = 0; i < 4 ; i++){
+        for(int j = 0 ; j < 4 ; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void print2(){
+    for(int i = 0 ; i < 4 ; i++){
+        for(int j = 0 ; j <= i; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void print3(){
+    for(int i = 1; i < 7 ; i++){
+        for(int j = 1; j < i; j++){
+            cout << j << " ";
+        } cout << endl;
+    }
+}
+
+void print4(){
+    for(int i = 0; i < 7 ; i++){
+        for(int j = 0; j <= i; j++){
+            cout << i << " ";
+        } cout << endl;
+    }
+}
+
+void print5(){
+    for(int i = 0; i < 5 ; i++){
+        for(int j = 0; j < 5-i; j++){
+            cout << " * ";
+        } 
+        cout << endl;
+    }
+}
+
+void print6(){
+    for(int i = 1; i < 7 ; i++){
+        for(int j = 1; j < 7 - i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print7(){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0 ; j < 9-i-1; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < 2*i+1; j++){
+            cout << "*";
+        }
+        for(int j = 0; j < 9-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main(){                         
+
+    print7();
+    return 0;
+}
+
+//        *        
+//       ***       
+//      *****      
+//     *******     
+//    *********    
+//   ***********   
+//  *************  
+// *************** 
+//*****************
+
+
