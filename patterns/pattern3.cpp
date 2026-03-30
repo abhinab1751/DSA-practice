f4d868407e465dc168b13ce268dde3bee46aa879
abhinab <abhinab1751@gmail.com>
2026-03-30 16:24:12 +0530

    pattern3

---
+++ patterns/pattern3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print1(){
+    for(int i = 0; i < 4 ; i++){
+        for(int j = 0 ; j < 4 ; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void print2(){
+    for(int i = 0 ; i < 4 ; i++){
+        for(int j = 0 ; j <= i; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void print3(){
+    for(int i = 1; i < 7 ; i++){
+        for(int j = 1; j < i; j++){
+            cout << j << " ";
+        } cout << endl;
+    }
+}
+
+int main(){                         
+
+    print3();
+    return 0;
+}
