7f1a177920e839542e9ae4ca0409e7ba28e36cb5
abhinab <abhinab1751@gmail.com>
2026-04-03 18:53:51 +0530

    pattern11

---
+++ patterns/pattern10.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print1(){
+    for(int i = 0; i < 4 ; i++){
+        for(int j = 0 ; j < 4 ; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void print2(){
+    for(int i = 0 ; i < 4 ; i++){
+        for(int j = 0 ; j <= i; j++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void print3(){
+    for(int i = 1; i < 7 ; i++){
+        for(int j = 1; j < i; j++){
+            cout << j << " ";
+        } cout << endl;
+    }
+}
+
+void print4(){
+    for(int i = 0; i < 7 ; i++){
+        for(int j = 0; j <= i; j++){
+            cout << i << " ";
+        } cout << endl;
+    }
+}
+
+void print5(){
+    for(int i = 0; i < 5 ; i++){
+        for(int j = 0; j < 5-i; j++){
+            cout << " * ";
+        } 
+        cout << endl;
+    }
+}
+
+void print6(){
+    for(int i = 1; i < 7 ; i++){
+        for(int j = 1; j < 7 - i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print7(){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0 ; j < 9-i-1; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < 2*i+1; j++){
+            cout << "*";
+        }
+        for(int j = 0; j < 9-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print8(){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0 ; j < i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < (2*9) - (2*i+1); j++){
+            cout << "*";
+        }
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print10(){
+    for(int i = 1; i < 2*8-1; i++){
+        int stars = i;
+        if(i > 8) stars = 2*8 - i;
+        for(int j = 1; j<=2*8 - i){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+
+
+int main(){                         
+    print7();
+    print8();
+    return 0;
+}
+
+
+//        *        
+//       ***       
+//      *****      
+//     *******     
+//    *********    
+//   ***********   
+//  *************  
+// *************** 
+//*****************
+//*****************
+// *************** 
+//  *************  
+//   ***********   
+//    *********    
+//     *******     
+//      *****      
+//       ***       
+//        *        
