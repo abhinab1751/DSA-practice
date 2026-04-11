2098f8282f89d5655e30134d3e6546f91ce39ec0
abhinab <abhinab1751@gmail.com>
2026-04-11 15:29:08 +0530

    iterators

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -22,5 +22,23 @@ void vectors(){
     vector<int> v1(5,20);     //{20,20,20,20,20}
     vector<int> v2(v1);
 
+    // iterators
+    vectors<int>::iterator it = v.begin();       //[2,6,8,4,1] will point to memory location of 2 
+
+    it++;
+    cout << *(it) << " ";      //now from address of 2 it will point to actual value which is 2 and then from it++ it will point to 6.
+    
+    it = it + 2; // here make it +2 from the begin then the answer will be 8
+    cout << *(it) << " ";
+    vector<int>:: iterators it = v.end();
+    vector<int>:: iterators it = v.rend();
+    vector<int>:: iterators it = v.rbegin();
+
+    cout << v[0] << " " << v.at(0);
+    cout << v.back() << " ";
+    
+
+    
+
 
 }
