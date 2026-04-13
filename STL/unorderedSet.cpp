300b15eeede8b3ac5c4a915a7cd3f88189a478c4
abhinab <abhinab1751@gmail.com>
2026-04-13 07:19:44 +0530

    unorderedSet

---
+++ STL/unorderedSet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void unorderedSet(){
+    unordered_set<int> un;
+    //lowerBound and upper_bound function
+    //does not works, rest all funtions are same
+    //as above , it does not stores in any 
+    //particular order it has better complexity 
+    //then set in most cases , except some when collision happens 
+}
