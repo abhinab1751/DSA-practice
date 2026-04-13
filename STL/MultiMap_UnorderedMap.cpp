44dfaae20cc07afc569bfb35497f5862b3d46136
abhinab <abhinab1751@gmail.com>
2026-04-13 09:01:05 +0530

    multimap&unorderedMap

---
+++ STL/MultiMap_UnorderedMap.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void MultiMap(){
+    //everything same as map , only it can store multiple keys 
+    //only map[key] cannot use here
+}
+
+void UnorderedMap(){
+    //Same as Unordered Set 
+}  
