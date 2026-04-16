66587651d73160e330c860c061fc558b56d8b5da
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-04-16 23:50:52 +0530

    Add closestEqual function to handle queries
    Implement a solution to find closest equal indices for given queries.

---
+++ binarySearchTree/closestEqual.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> pos;
+        
+        for (int i = 0; i < n; i++) {
+            pos[nums[i]].push_back(i);
+        }
+        
+        vector<int> ans(n, -1);
+        
+        for (auto& [val, indices] : pos) {
+            int k = indices.size();
+            if (k > 1) {
+                for (int i = 0; i < k; i++) {
+                    int curr = indices[i];
+                    
+                    int prev_idx = indices[(i - 1 + k) % k];
+                    int next_idx = indices[(i + 1) % k];
+                    
+                    int dist_prev = min(abs(curr - prev_idx), n - abs(curr - prev_idx));
+                    int dist_next = min(abs(curr - next_idx), n - abs(curr - next_idx));
+                    
+                    ans[curr] = min(dist_prev, dist_next);
+                }
+            }
+        }
+        
+        vector<int> result(queries.size());
+        for (int i = 0; i < queries.size(); i++) {
+            result[i] = ans[queries[i]];
+        }
+        
+        return result;
+    }
+};
