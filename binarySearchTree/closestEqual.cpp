5bdd9971e37cce09b14fe1afee866c424e02a47e
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-04-20 23:43:14 +0530

    Add maxDistance function to Solution class
    Implement maxDistance function to find maximum distance between different colors.

--- binarySearchTree/closestEqual.cpp
+++ binarySearchTree/closestEqual.cpp
@@ -38,6 +38,37 @@ public:
         for (int i = 0; i < queries.size(); i++) {
             result[i] = ans[queries[i]];
         }
+
+
+
+
+------------------------------------------------------------------------------------------->
+    #include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int maxDist = 0;
+        
+        for (int j = n - 1; j >= 0; j--) {
+            if (colors[j] != colors[0]) {
+                maxDist = j;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (colors[i] != colors[n - 1]) {
+                maxDist = max(maxDist, (n - 1 - i));
+                break;
+            }
+        }
+
+        return maxDist;
+    }
+};
         
         return result;
     }
