aba7797fff1b6a1a2a4bcd947c10503a791ce584
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-04-27 23:26:46 +0530

    Add hasValidPath function to check grid paths

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -572,3 +572,61 @@ public:
         return false;
     }
 };
+
+
+------------------------------------------------------------------------------------------------->
+
+    class Solution {
+public:
+    vector<vector<int>> dir = {
+        {},                
+        {0, 1, 0, -1},      
+        {1, 0, -1, 0},      
+        {0, -1, 1, 0},      
+        {0, 1, 1, 0},       
+        {0, -1, -1, 0},     
+        {0, 1, -1, 0}       
+    };
+
+    bool hasValidPath(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        queue<pair<int,int>> q;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+
+        q.push({0,0});
+        vis[0][0] = true;
+
+        while(!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            if(x == m-1 && y == n-1) return true;
+
+            int type = grid[x][y];
+
+            for(int i = 0; i < dir[type].size(); i += 2) {
+                int nx = x + dir[type][i];
+                int ny = y + dir[type][i+1];
+
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n || vis[nx][ny])
+                    continue;
+
+                int nextType = grid[nx][ny];
+
+                for(int j = 0; j < dir[nextType].size(); j += 2) {
+                    int backX = nx + dir[nextType][j];
+                    int backY = ny + dir[nextType][j+1];
+
+                    if(backX == x && backY == y) {
+                        vis[nx][ny] = true;
+                        q.push({nx, ny});
+                        break;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
