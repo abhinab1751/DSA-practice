44f9feb64d0637dc644fa71f903a35c22336fc46
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-04-30 23:45:46 +0530

    Add maxPathScore function to Solution class

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -678,3 +678,54 @@ public:
         return ans;
     }
 };
+
+--------------------------------------------------------------------------------------------->
+    class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        const int NEG = -1e9;
+
+        vector<vector<vector<int>>> dp(
+            m, vector<vector<int>>(n, vector<int>(k + 1, NEG))
+        );
+
+        dp[0][0][0] = 0;  
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) continue;
+
+                int addScore = grid[i][j];
+                int addCost = (grid[i][j] == 0 ? 0 : 1);
+
+                for (int c = addCost; c <= k; c++) {
+
+                    if (i > 0 && dp[i - 1][j][c - addCost] != NEG) {
+                        dp[i][j][c] = max(
+                            dp[i][j][c],
+                            dp[i - 1][j][c - addCost] + addScore
+                        );
+                    }
+
+                    if (j > 0 && dp[i][j - 1][c - addCost] != NEG) {
+                        dp[i][j][c] = max(
+                            dp[i][j][c],
+                            dp[i][j - 1][c - addCost] + addScore
+                        );
+                    }
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int c = 0; c <= k; c++) {
+            ans = max(ans, dp[m - 1][n - 1][c]);
+        }
+
+        return ans < 0 ? -1 : ans;
+    }
+};
