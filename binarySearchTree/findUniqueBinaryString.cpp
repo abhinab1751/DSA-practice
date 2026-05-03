f1f5ac3643c399728cc390a1216cb77ce55ebc78
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-05-03 23:11:15 +0530

    Add rotateString function to Solution class

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -775,3 +775,14 @@ public:
         return count;
     }
 };
+
+------------------------------------------------------------------------------------------------------------------------------>
+    class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        
+        string temp = s + s;
+        return temp.find(goal) != string::npos;
+    }
+};
