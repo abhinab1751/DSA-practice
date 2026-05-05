f3103d3f74eb0ed33fd02b3347c9362cb10c334a
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-05-05 23:58:12 +0530

    Implement rotateRight function for linked list

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -804,3 +804,32 @@ public:
         }
     }
 };
+----------------------------------------------------------------------------------------------------------------------->
+    class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+
+        ListNode* temp = head;
+        int n = 1;
+        while (temp->next) {
+            temp = temp->next;
+            n++;
+        }
+
+        temp->next = head;
+
+        k = k % n;
+
+        int steps = n - k;
+        ListNode* newTail = head;
+        for (int i = 1; i < steps; i++) {
+            newTail = newTail->next;
+        }
+
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
+        return newHead;
+    }
+};
