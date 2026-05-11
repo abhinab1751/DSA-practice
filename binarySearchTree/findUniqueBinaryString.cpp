e39d881c30207fe70fa18780c1f1542049250f43
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-05-11 23:46:11 +0530

    Add separateDigits function to convert nums to digits

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -864,3 +864,20 @@ public:
         return result;
     }
 };
+----------------------------------------------------------------------------------------------->
+    class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+
+        for (int num : nums) {
+            string s = to_string(num);
+
+            for (char ch : s) {
+                ans.push_back(ch - '0');
+            }
+        }
+
+        return ans;
+    }
+};
