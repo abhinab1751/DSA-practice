26029a1c69698d92ae0f90044866158387022573
Abhinaba Dey <149684407+abhinab1751@users.noreply.github.com>
2026-05-17 22:18:18 +0530

    Add DFS and canReach methods to Solution class

--- binarySearchTree/findUniqueBinaryString.cpp
+++ binarySearchTree/findUniqueBinaryString.cpp
@@ -1012,5 +1012,28 @@ public:
 };
 
 
+class Solution {
+public:
+    bool dfs(vector<int>& arr, int idx, vector<int>& vis) {
+        int n = arr.size();
+
+        if (idx < 0 || idx >= n) return false;
+
+        if (vis[idx]) return false;
+
+        if (arr[idx] == 0) return true;
+
+        vis[idx] = 1;
+
+        return dfs(arr, idx + arr[idx], vis) ||
+               dfs(arr, idx - arr[idx], vis);
+    }
+
+    bool canReach(vector<int>& arr, int start) {
+        vector<int> vis(arr.size(), 0);
+        return dfs(arr, start, vis);
+    }
+};
+
 
 
